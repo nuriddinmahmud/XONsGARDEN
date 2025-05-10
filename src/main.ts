18a1ed24e4813adba0398c4c50bfd4679fffac3b
Nuriddin Mahmud <154328556+nuriddinmahmud@users.noreply.github.com>
2025-05-10 14:41:44 +0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', 
+    credentials: true,              
+  });
 
   const config = new DocumentBuilder()
     .setTitle("XON's Garden API")
